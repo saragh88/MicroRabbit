fe596b0ca0ec4872eea7711be6abff38756366c8
sara ghasemi <s.ghasemi@iranargham.com>
2021-08-09 11:02:44 +0430

    Partially completed Banking MicroService

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController : ControllerBase
+    {
+      
+
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Banking.Domain/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string AccountType { get; set; }
+
+        [Column(TypeName = "decimal(18,0)")]
+        public decimal AccountBalance { get; set; }
+    }
+
+}
